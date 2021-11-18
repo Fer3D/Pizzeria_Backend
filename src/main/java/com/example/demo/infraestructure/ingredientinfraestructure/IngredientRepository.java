873a0fa8dadfd7f79adf9f80a7efd99f360907fe
AidaSFuentes <aidadesarrolloweb@gmail.com>
2021-11-18 11:06:33 +0100

    updated

--- src/main/java/com/example/demo/infraestructure/ingredientinfraestructure/IngredientRepository.java
+++ src/main/java/com/example/demo/infraestructure/ingredientinfraestructure/IngredientRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.infraestructure.ingredientinfraestructure;
 
 import java.util.UUID;
 
+import com.example.demo.IngredientDTO.IngredientDTO;
+import com.example.demo.application.ingredientApplication.IngredientApplication;
 import com.example.demo.domain.ingredientDomain.Ingredient;
-import com.example.demo.domain.ingredientDomain.IngredientProjection;
+
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Query;
@@ -14,10 +16,12 @@ import reactor.core.publisher.Mono;
 
 public interface IngredientRepository extends ReactiveCrudRepository<Ingredient, UUID> {
     @Query("SELECT id, name, price FROM Ingredient WHERE (:name is NULL OR name LIKE '%:name%')")
-    Flux<IngredientProjection> findByName(String name, Pageable pageable); /*Spring Boot usa el complemento Pageable para implementar paginación y consulta. 
+    Flux<IngredientApplication> findByName(String name, Pageable pageable); /*Spring Boot usa el complemento Pageable para implementar paginación y consulta. 
     //Una interfaz definida en la biblioteca de datos. ... A través de esta interfaz, podemos obtener toda la información relacionada con la paginación 
     (como pageNumber, pageSize, etc. .).*/
 
     @Query("SELECT CASE WHEN COUNT(ingredient)>0 THEN true ELSE false END FROM ingredient WHERE name = :name")
     Mono<Boolean> existsByName(String name);
+
+    Mono<IngredientDTO> add(Ingredient ingredient);
 }
