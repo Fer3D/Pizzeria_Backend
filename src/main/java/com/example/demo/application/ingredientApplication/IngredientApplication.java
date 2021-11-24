c77c8c0aa4b45adfbfd737e67a2f64a248688084
AidaSFuentes <aidadesarrolloweb@gmail.com>
2021-11-24 13:33:49 +0100

    testing adding ingredients

--- src/main/java/com/example/demo/application/ingredientApplication/IngredientApplication.java
+++ src/main/java/com/example/demo/application/ingredientApplication/IngredientApplication.java
@@ -3,7 +3,5 @@ package com.example.demo.application.ingredientApplication;
 import reactor.core.publisher.Mono;
 
 public interface IngredientApplication {
-    
     public Mono<IngredientDTO> add(CreateOrUpdateIngredientDTO dto);
-   
 }
--- src/main/java/com/example/demo/core/configurationBeans/MapperConfiguration.java
+++ src/main/java/com/example/demo/core/configurationBeans/MapperConfiguration.java
@@ -3,9 +3,10 @@ package com.example.demo.core.configurationBeans;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+@Configuration
 public class MapperConfiguration {
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
