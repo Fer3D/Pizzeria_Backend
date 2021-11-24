5ded22b184c65af6cb72747f8808a3e706784dee
Srtagalicia <srtagalicia@gmail.com>
2021-11-24 18:19:02 +0100

    Controller fixed

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import com.example.demo.application.userApplication.CreateUserDTO;
 import com.example.demo.application.userApplication.UpdateUserDTO;
 import com.example.demo.application.userApplication.UserApplication;
 import com.example.demo.application.userApplication.UserDTO;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,6 +36,12 @@ public class UserController {
         this.userApplication = userApplication;
     }
     
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<UserDTO>> create(@Valid @RequestBody CreateUserDTO dto) {
+        Mono<UserDTO> userDTO = this.userApplication.add(dto);
+        return userDTO.map(user -> ResponseEntity.ok(user)).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
     public Mono<ResponseEntity<UserDTO>> update(@Valid @PathVariable UUID id, @Valid @RequestBody UpdateUserDTO dto) {
         Mono<UserDTO> userDTO = this.userApplication.update(id, dto);
