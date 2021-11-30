51e8dc3790d21a5611827f3abf5b3f5f006ba489
AidaSFuentes <aidadesarrolloweb@gmail.com>
2021-11-30 12:40:24 +0100

    added flux projection with parameters ok

--- src/main/java/com/example/demo/application/ingredientApplication/IngredientApplication.java
+++ src/main/java/com/example/demo/application/ingredientApplication/IngredientApplication.java
@@ -1,18 +1,15 @@
 package com.example.demo.application.ingredientApplication;
 
 import java.util.UUID;
-
-import javax.validation.Valid;
-
 import com.example.demo.domain.ingredientDomain.IngredientProjection;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface IngredientApplication {
     public Mono<IngredientDTO> add(CreateOrUpdateIngredientDTO dto);
-    public Mono<IngredientDTO> get(@Valid UUID id);
+    public Mono<IngredientDTO> get(UUID id);
     public Mono<IngredientDTO> update(UUID id, CreateOrUpdateIngredientDTO dto);
     public Mono<Void> delete(UUID id);
     public Flux<IngredientProjection> getAll(String name,  int page, int size);
+
 }
