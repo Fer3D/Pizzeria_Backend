90112531ae895f0723e3d459615ec6db3b698558
AidaSFuentes <aidadesarrolloweb@gmail.com>
2021-11-30 12:43:22 +0100

    added @FunctionalInterface

--- src/main/java/com/example/demo/core/functionalInterfaces/FindById.java
+++ src/main/java/com/example/demo/core/functionalInterfaces/FindById.java
@@ -2,6 +2,7 @@ package com.example.demo.core.functionalInterfaces;
 
 import reactor.core.publisher.Mono;
 
+@FunctionalInterface
 public interface FindById<T, ID> {
     public Mono<T> findById(ID id);   
 }
